176921ec2408f9050d163de3043782bb9f903804
Gihun Ham <progh2@gmail.com>
2019-05-28 11:07:30 +0900

    #2 PictureBox, ImageList 동작 코드 구현

--- ImageViewer/Form1.cs
+++ ImageViewer/Form1.cs
@@ -11,9 +11,41 @@ namespace ImageViewer
 {
     public partial class Form1 : Form
     {
+        int imgCount = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Show();
+        }
+
+        private void Show()
+        {
+            this.picImg.Image = this.imgList.Images[imgCount];
+            this.lblNum.Text = (imgCount + 1) + "/" + this.imgList.Images.Count;
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            this.imgCount++;
+            if (this.imgCount >= this.imgList.Images.Count)
+            {
+                this.imgCount = 0;
+            }
+            Show();
+        }
+
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            this.imgCount--;
+            if(this.imgCount < 0)
+            {
+                this.imgCount = this.imgList.Images.Count - 1;
+            }
+            Show();
         }
     }
 }
